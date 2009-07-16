6e3d4c341c99d868e177a193130b010030008b47
jgarnett <jgarnett@e5c1c795-43da-0310-a71f-fac65c449510>
2009-07-16 19:21:52 +0000

    working function list completion
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31359 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.style.sld/src/net/refractions/udig/filter/ExpressionViewer.java
+++ plugins/net.refractions.udig.style.sld/src/net/refractions/udig/filter/ExpressionViewer.java
@@ -1,9 +1,14 @@
 package net.refractions.udig.filter;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.eclipse.jface.fieldassist.ContentProposalAdapter;
 import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
-import org.eclipse.jface.fieldassist.SimpleContentProposalProvider;
+import org.eclipse.jface.fieldassist.IContentProposal;
+import org.eclipse.jface.fieldassist.IContentProposalProvider;
 import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -16,12 +21,14 @@ import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.FunctionFinder;
 import org.geotools.filter.text.cql2.CQL;
 import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
 import org.geotools.util.Utilities;
 import org.opengis.filter.expression.Expression;
+import org.opengis.filter.expression.Function;
 
 /**
  * A JFace Style Expression Viewer that can be used to show an Expression
@@ -74,7 +81,7 @@ public class ExpressionViewer extends Viewer {
             String before = expr != null ? CQL.toCQL( expr ) : "(empty)";
             validate();
             String after = expr != null ? CQL.toCQL( expr ) : "(empty)";
-            if( !Utilities.equals( before, after )){
+            if( expr != null && !Utilities.equals( before, after )){              
                 fireSelectionChanged( new SelectionChangedEvent( ExpressionViewer.this, getSelection() ));                
             }
         }
@@ -108,9 +115,11 @@ public class ExpressionViewer extends Viewer {
         
         FunctionFinder ff = new FunctionFinder(null);
 
+        FunctionContentProposalProvider proposalProvider = new FunctionContentProposalProvider();
+        proposalProvider.setFiltering(true);
         ContentProposalAdapter adapter = new ContentProposalAdapter(
                 text, new TextContentAdapter(), 
-                new SimpleContentProposalProvider(new String [] {"ProposalOne", "ProposalTwo", "ProposalThree"}),
+                proposalProvider,
                 null, null);
         
         text.addKeyListener(keyListener);
@@ -322,3 +331,136 @@ public class ExpressionViewer extends Viewer {
         }
     }
 }
+
+/**
+ * SimpleContentProposalProvider is a class designed to map a static list of
+ * Strings to content proposals.
+ * 
+ * @see IContentProposalProvider
+ * @since 3.2
+ * 
+ */
+@SuppressWarnings("deprecation")
+class FunctionContentProposalProvider implements IContentProposalProvider {
+
+    public static Set<String> proposals;
+    static {
+        proposals = new TreeSet<String>();
+        Set<Function> oldFunctions = CommonFactoryFinder.getFunctionExpressions(null);   
+        for( Function function : oldFunctions ){
+            proposals.add( function.getName().toLowerCase() );
+        }
+        Set<Function> newFunctions = CommonFactoryFinder.getFunctions(null);
+        for( Function function : newFunctions ){
+            proposals.add( function.getName().toLowerCase() );
+        }
+    }
+    
+    /*
+     * The proposals mapped to IContentProposal. Cached for speed in the case
+     * where filtering is not used.
+     */
+    private IContentProposal[] contentProposals;
+
+    /*
+     * Boolean that tracks whether filtering is used.
+     */
+    private boolean filterProposals = false;
+
+    /**
+     * Construct a SimpleContentProposalProvider whose content proposals are
+     * always the specified array of Objects.
+     * 
+     * @param proposals
+     *            the array of Strings to be returned whenever proposals are
+     *            requested.
+     */
+    public FunctionContentProposalProvider() {
+    }
+
+    /**
+     * Return an array of Objects representing the valid content proposals for a
+     * field. 
+     * 
+     * @param contents
+     *            the current contents of the field (only consulted if filtering
+     *            is set to <code>true</code>)
+     * @param position
+     *            the current cursor position within the field used to select a word
+     * @return the array of Objects that represent valid proposals for the field
+     *         given its current content.
+     */
+    public IContentProposal[] getProposals(String contents, int position) {
+        String word = contents.substring(0,position);
+        int start = contents.lastIndexOf(" ", position);
+        if( start != -1 ){
+            word = contents.substring( start, position );
+        }
+        word = word.trim();
+        if( word.length() == 0 ){
+            return new IContentProposal[0];
+        }
+        
+        if (filterProposals) {
+            ArrayList<IContentProposal> list = new ArrayList<IContentProposal>();
+            for( String proposal : proposals ) {
+                if (proposal.length() >= word.length()
+                        && proposal.substring(0, word.length())
+                                .equalsIgnoreCase(word)) {
+                    list.add(makeContentProposal(proposal));
+                }
+            }
+            return (IContentProposal[]) list.toArray(new IContentProposal[list
+                    .size()]);
+        }
+        if (contentProposals == null) {
+            contentProposals = new IContentProposal[proposals.size()];
+            int i = 0;
+            for( String proposal : proposals ){
+                contentProposals[i] = makeContentProposal(proposal);
+            }
+        }
+        return contentProposals;
+    }
+
+    /**
+     * Set the boolean that controls whether proposals are filtered according to
+     * the current field content.
+     * 
+     * @param filterProposals
+     *            <code>true</code> if the proposals should be filtered to
+     *            show only those that match the current contents of the field,
+     *            and <code>false</code> if the proposals should remain the
+     *            same, ignoring the field content.
+     * @since 3.3
+     */
+    public void setFiltering(boolean filterProposals) {
+        this.filterProposals = filterProposals;
+        // Clear any cached proposals.
+        contentProposals = null;
+    }
+
+    /*
+     * Make an IContentProposal for showing the specified String.
+     */
+    private IContentProposal makeContentProposal(final String proposal) {
+        return new IContentProposal() {
+            public String getContent() {
+                return proposal;
+            }
+
+            public String getDescription() {
+                return null;
+            }
+
+            public String getLabel() {
+                return null;
+            }
+
+            public int getCursorPosition() {
+                return proposal.length();
+            }
+        };
+    }
+}
+
