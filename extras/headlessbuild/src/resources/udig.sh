16b187f252ab08e027e01315cc8b85d956646ab8
aantonello <aantonello@e5c1c795-43da-0310-a71f-fac65c449510>
2009-09-09 11:10:55 +0000

    added imageio-ext environmental variables to the linuxlaunch script. Added it is udig.sh for name compatibility with the bat file
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31479 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ extras/headlessbuild/src/resources/udig.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+PRG="$0"
+while [ -h "$PRG" ]; do
+	ls=`ls -ld "$PRG"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '/.*' > /dev/null; then
+		PRG="$link"
+	else
+		PRG=`dirname "$PRG"`/"$link"
+	fi
+done
+
+# Add imageio-ext variables
+PWD=`pwd`
+export GDAL_DATA="$PWD/gdal_data"
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+DATA_ARG=false
+
+for ARG in $@ 
+do
+	if [ $ARG = "-data" ]; then DATA_ARG=true; fi
+done
+
+if $DATA_ARG; then 
+	$PRGDIR/bin $@
+else
+	$PRGDIR/bin -data ~/uDigWorkspace $@
+fi
